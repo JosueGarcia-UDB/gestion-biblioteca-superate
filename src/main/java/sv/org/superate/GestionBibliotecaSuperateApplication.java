2e1c9a53bbdc8822f5e14b2c046f9f951b64cded
Josue Garcia <GE240098@alumno.udb.edu.sv>
2025-03-07 12:30:40 -0600

    Init project with SpringBoot

---
+++ src/main/java/sv/org/superate/GestionBibliotecaSuperateApplication.java
@@ -0,0 +1,13 @@
+package sv.org.superate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBibliotecaSuperateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionBibliotecaSuperateApplication.class, args);
+    }
+
+}
